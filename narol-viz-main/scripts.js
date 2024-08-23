cd94ad4507839c3baa66ca4f3af87128af0f02da
R3No <misi0140@gmail.com>
2024-08-23 10:42:30 +0200

    Update scripts.js

--- narol-viz-main/scripts.js
+++ narol-viz-main/scripts.js
@@ -18,3 +18,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Vágolapra másolva: " + text);
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+}
+
+// Event listeners for the entire info-card box
+document.getElementById('email-card').addEventListener('click', function() {
+    const emailText = this.querySelector('p').textContent;
+    copyToClipboard(emailText);
+});
+
+document.getElementById('phone-card').addEventListener('click', function() {
+    const phoneText = this.querySelector('p').textContent;
+    copyToClipboard(phoneText);
+});
